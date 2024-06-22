ffc0bc57bd8457b7ea2b138371c2475b555ba7bc
陳廣旂 <chi_19911231@outlook.com>
2024-06-22 23:50:55 +0800

    新增底選單

---
+++ WebApplicationBase/ViewComponents/FooterMenu.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationBase.ViewComponents
+{
+    public class FooterMenu : ViewComponent
+    {
+        public FooterMenu()
+        {
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var items = await _context.Category.ToListAsync();
+            return View();
+        }
+    }
+}
