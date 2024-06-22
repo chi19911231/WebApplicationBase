562a913967f1f353a7537c68c6acbd0b683a4582
陳廣旂 <chi_19911231@outlook.com>
2024-06-22 23:21:09 +0800

    新增上排選單

---
+++ WebApplicationBase/ViewComponents/TopMenu.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationBase.ViewComponents
+{
+    public class TopMenu : ViewComponent
+    {       
+        public TopMenu()
+        {
+            
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var items = await _context.Category.ToListAsync();
+            return View();
+        }
+    }
+}
