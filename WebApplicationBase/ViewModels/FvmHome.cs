d25afb9707eb7bee72fd61a06bdc5051a041e9ae
陳廣旂 <chi_19911231@outlook.com>
2024-06-23 23:11:12 +0800

    建置ViewModels層

---
+++ WebApplicationBase/ViewModels/FvmHome.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApplicationBase.ViewModels
+{
+    /// <summary>
+    /// 範本
+    /// </summary>
+    public class FvmHome
+    {
+        /// <summary>
+        /// 範本 模型
+        /// </summary>
+        public class VM_Data
+        {
+            /// <summary> ID </summary>
+            public int ID { get; set; }
+
+            /// <summary> 標題 </summary>
+            public string Title { get; set; }
+
+            /// <summary> 內容 </summary>
+            public string Content { get; set; }
+
+        }
+    }
+}
