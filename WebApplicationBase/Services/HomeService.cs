30def10b1c6057d428b4ec243a50819ad32b4a48
陳廣旂 <chi_19911231@outlook.com>
2024-06-23 16:43:34 +0800

    建置Services層

---
+++ WebApplicationBase/Services/HomeService.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplicationBase.Services
+{
+    /// <summary>
+    /// 主頁 Interface
+    /// </summary>
+    public interface IHomeService
+    {
+        
+    }
+
+    /// <summary>
+    /// 主頁 Service
+    /// </summary>
+    public class HomeService : IHomeService
+    {
+
+    }
+}
