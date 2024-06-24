711d86aa21dd7e73f591bbdad48d142280cdf113
陳廣旂 <chi_19911231@outlook.com>
2024-06-24 22:06:49 +0800

    新增以下項目 1.Program注入DI、IOC 2.HomeController注入DI、IOC 3.新增View 4.View新增@model ViewMode

--- WebApplicationBase/Controllers/HomeController.cs
+++ WebApplicationBase/Controllers/HomeController.cs
@@ -1,21 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebApplicationBase.Models;
+using WebApplicationBase.Services;
+using WebApplicationBase.ViewModels;
 
 namespace WebApplicationBase.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHomeService _homeServicer;
 
-        public HomeController(ILogger<HomeController> logger)
+        #region 建構子
+        /// <summary>
+        /// 建構子
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="homeService"></param>
+        public HomeController(ILogger<HomeController> logger , IHomeService homeService)
         {
             _logger = logger;
+            _homeServicer = homeService;
         }
+        #endregion
+
 
         public IActionResult Index()
-        {
-
+        {            
             return View();
         }
 
@@ -29,5 +40,24 @@ namespace WebApplicationBase.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        /// <summary> 取得所有資料 </summary>
+        /// <returns></returns>
+        public IActionResult GetListData()
+        {
+            var model = _homeServicer.GetSearchList();
+            return View(model);
+        }
+
+        /// <summary> 取得單筆資料 </summary>
+        /// <param name="id"> 資料編號 </param>
+        /// <returns></returns>
+        public async Task<IActionResult> GetData(int id)
+        {
+            var model = await _homeServicer.GetAsync(id);
+            return View(model);
+        }
+
     }
 }
--- WebApplicationBase/Program.cs
+++ WebApplicationBase/Program.cs
@@ -1,7 +1,27 @@
+using WebApplicationBase.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//新增服務(Add services to the container.)
+//註冊 MVC 功能所需的服務，包括路由、控制器
 builder.Services.AddControllersWithViews();
+//註冊內存快取服務
+//builder.Services.AddMemoryCache();
+
+//HomeService 註冊為 Singleton 
+//Singleton(單例)：只建立一個新的，每次都重複利用同一個
+builder.Services.AddSingleton<IHomeService, HomeService>();
+
+//HomeService 註冊為 Scope 
+//Scoped(作用域)：每次 Request 都建立一個新的，同個 Request 重複利用同一個
+//builder.Services.AddScoped<IHomeService, HomeService>();
+
+//HomeService 註冊為 Transient 
+//Transient(一次性)：每次注入都建立一個新的
+//builder.Services.AddTransient<IHomeService, HomeService>();
+
+
+
 
 var app = builder.Build();
 
