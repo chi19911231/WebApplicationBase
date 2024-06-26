4af7d3c9edbbd2a791899aefd29c4732fcccc8c5
陳廣旂 <chi_19911231@outlook.com>
2024-06-26 17:04:38 +0800

    新增EntityFramework 1.安装Entity Framework 8.0.6版 2.安装EntityFrameworkCore.Tools 8.0.6版 3.安装Entity Framework Core 8.0.6版 4.安装EntityFrameworkCore.Design 8.0.6版

---
+++ WebApplicationBase/Models/Entities/WebApplicationBaseContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationBase.Models.Entities;
+
+public partial class WebApplicationBaseContext : DbContext
+{
+    public WebApplicationBaseContext()
+    {
+    }
+
+    public WebApplicationBaseContext(DbContextOptions<WebApplicationBaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<BaseTable> BaseTables { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-4NO5S2N;Database=WebApplicationBase;User ID=sa;Password=1qaz@WSX;MultipleActiveResultSets=true;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BaseTable>(entity =>
+        {
+            entity.ToTable("BaseTable");
+
+            entity.Property(e => e.Id)
+                .HasComment("ID")
+                .HasColumnName("ID");
+            entity.Property(e => e.Content).HasMaxLength(100);
+            entity.Property(e => e.CreateTime)
+                .HasComment("建立時間")
+                .HasColumnType("datetime");
+            entity.Property(e => e.CreateUser).HasComment("建立使用者");
+            entity.Property(e => e.Title).HasMaxLength(20);
+            entity.Property(e => e.UpdateTime)
+                .HasComment("異動時間")
+                .HasColumnType("datetime");
+            entity.Property(e => e.UpdateUser).HasComment("更新使用者");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
